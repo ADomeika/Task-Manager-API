b083dbf76f40999581dbdffa86d7807b86d31b0e
Adomas Domeika <adomas.domeika@jet2.com>
2019-05-01 15:42:47 +0100

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,21 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+exports.sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'adomeika94@gmail.com',
+    subject: 'Welcome to the Task Manager',
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+  })
+}
+
+exports.sendCancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'adomeika94@gmail.com',
+    subject: 'Sorry to see you go!',
+    text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+  })
+}
